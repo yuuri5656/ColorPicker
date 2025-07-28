09ccb2af3c25f81059eb4be22607893cb46254a3
yuuri5656 <109333851+yuuri5656@users.noreply.github.com>
2025-07-28 01:17:41 +0000

    バグ修正用

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert(document.querySelector("#colorPicker"));
+// colorText及びcolorPickerを定数化
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+// colorBgという関数を作成
+const colorBg = () => {
+  // 背景色を入力された色に変更する
+  document.body.style.backgroundColor = color.value
+
+  // カラーコードを表示
+  if (color.value === '#ffffff') {
+    text.textContent = `ColorCode: ${color.value} (white)`;
+  } else {
+    text.textContent = `ColorCode: ${color.value`;
+  }
+}
+
+color.addEventListener('input', colorBg);
